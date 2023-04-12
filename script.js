db7c8f5a4259ca6902d48b0e89e356858f37cb9d
Nassim Bourebia <b.nassim01@gmail.com>
2023-04-12 16:18:08 +0200

    add js and style

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("connecté");
+const icone = document.querySelector('i');
+console.log(icone);
+
+icone.addEventListener('click', function(){
+    console.log('icone cliqué ');
+    icone.classList.toggle("fa-face-smile");
+    icone.classList.toggle('happy')
+})
