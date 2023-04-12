c8e37e7a05a787f86f03eea44cc37e9e7b035127
Nassim Bourebia <b.nassim01@gmail.com>
2023-04-12 16:31:44 +0200

    add condition for btn

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const icone = document.querySelector('i');
-console.log(icone);
+const btnButton = document.querySelector('.btn')
+
+btnButton.addEventListener('click', function(){
+    //On vérifie le texte du bouton
+    if(btnButton.innerText === "Abonné") {
+    //Action à réaliser si la condition est vraie
+        btnButton.innerText = "Abonnez-vous"; 
+    } else{
+    //Action à réaliser si la condition est fausse
+    btnButton.innerText = "Abonné"
+    }
+  ;
+})
 
 icone.addEventListener('click', function(){
     console.log('icone cliqué ');
